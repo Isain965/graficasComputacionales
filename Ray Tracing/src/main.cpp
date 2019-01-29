b12dbc3e700f5df48a842572ca770633a9186af5
Isain <isain.cuadra@konfio.mx>
2019-01-29 16:18:15 -0600

    chapter 7

--- Ray Tracing/src/main.cpp
+++ Ray Tracing/src/main.cpp
@@ -13,10 +13,22 @@
 
 float MAXFLOAT = 10.0;
 
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		float random1 = ((double)rand() / (RAND_MAX));
+		float random2 = ((double)rand() / (RAND_MAX));
+		float random3 = ((double)rand() / (RAND_MAX));
+		p = 2.0*vec3(random1, random2, random3) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5*color( ray(rec.p, target-rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -55,6 +67,7 @@ int main() {
 				col += color(r, world);
 			}
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
 			//Imprimir todo rojo en la pantalla
 			pixels.push_back(int(255.99 * col[0]));
