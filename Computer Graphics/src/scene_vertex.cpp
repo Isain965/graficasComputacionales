6aeb7d66a7069c913a3f1ec45ed4ab51a006b074
Isain <isain.cuadra@konfio.mx>
2019-03-08 16:28:53 -0600

    shaders profr

--- Computer Graphics/src/scene_vertex.cpp
+++ Computer Graphics/src/scene_vertex.cpp
@@ -1,47 +1,105 @@
-#include "..\include\scene_vertex.h"
+﻿#include "scene_vertex.h"
 
 #include "ifile.h"
 
-scene_vertex::~scene_vertex()
-{
+scene_vertex::~scene_vertex() {
+
+	// Borramos la memoria del ejecutable cuando
+	// la escena deja de existir
+	glDeleteProgram(shader_program);
 }
 
-void scene_vertex::init() {
-
+void scene_vertex::init()
+{
+	// ifile es parte del codigo que yo les doy
+	// El codigo fuente se encuentra en el proyecto Util
+	// Su unico proposito en la vida es leer archivos de texto
 	ifile shader_file;
+	// El metodo read recibe la ruta al archivo de texto a leer
+	// Si encuentra el archivo, intenta leerlo. En este caso,
+	// estamos intentando leer un archivo llamado grid,
+	// dentro de una carpeta shaders.
 	shader_file.read("shaders/grid.vert");
+	// Obtenemos los contenidos leidos en el paso anterior
+	// utilizando el metodo get_contents. Regresa un string
 	std::string vertex_source = shader_file.get_contents();
+	// OpenGL es un API de C, por lo que no trabaja con
+	// strings de C++. Tenemos que hacer un cast a un tipo de
+	// dato que OpenGL entienda. Podemos usar strings de C (char*)
+	// o utilizar el tipo de dato definido por OpenGL (GLchar*).
+	// Preferimos lo segundo.
 	const GLchar* vertex_source_c = (const GLchar*)vertex_source.c_str();
+	// Creamos el identificador para un vertex shader,
+	// utiliznado la funcion glCreateShader. La funcion
+	// regresa el identificador y lo guardamos en la variable
+	// vertex_shader.
 	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	// Utilizando la funcion glShaderSource, indicamos que queremos
+	// enviar el codigo fuente del shader. La funcion espera:
+	// Identificador del shader (vertex_shader)
+	// Cuantos codigos fuentes queremos manadar (1)
+	// El c�digo fuente (vertex_source_c)
+	// La longitud del codigo fuente. Si usamos nullptr, se
+	// asume que debe continuar leyendo hasta encontrar un nullptr.
 	glShaderSource(vertex_shader, 1, &vertex_source_c, nullptr);
+	// Compilamos el codigo fuente contenido en el shader
+	// con identificador vertex_shader.
 	glCompileShader(vertex_shader);
 
+	// Adicionalmente, habria que verificar que el proceso de
+	// compilacion ha sido completado de manera exitosa.
+	// Por tiempo, simplemente asumimos que el codigo no
+	// tenia errores.
+
+	// Repetimos el mismo proceso, pero ahora para un
+	// fragment shader contenido en un archivo llamado
+	// solid_color.frag dentro de la carpeta shaders.
 	shader_file.read("shaders/solid_color.frag");
-	std::string fragment_sourse = shader_file.get_contents();
-	const GLchar* fragment_source_c = (const GLchar*)fragment_sourse.c_str();
+	std::string fragment_source = shader_file.get_contents();
+	const GLchar* fragment_source_c = (const GLchar*)fragment_source.c_str();
+	// El identificador del shader lo creamos pero para un 
+	// shader de tipo fragment.
 	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment_shader, 1, &fragment_source_c, nullptr);
 	glCompileShader(fragment_shader);
 
+	// Una vez que hemos creado los shaders necesarios,
+	// creamos el manager utilizando la funcion glCreateProgram
+	// que regresa el id.
 	shader_program = glCreateProgram();
+	// Utilizamos glAttachShader para asociar un shader con el manager
+	// En este caso, shader_program es manager de vertex_shader
 	glAttachShader(shader_program, vertex_shader);
+	// En este caso, shader_program es manager de fragment_shader
 	glAttachShader(shader_program, fragment_shader);
+	// Ejecutamos el proceso de linkeo. En esta etapa se busca
+	// que los shaders puedan trabajar en conjunto y todo este
+	// definido correctamente.
 	glLinkProgram(shader_program);
 
+	// Tambien deberiamos verificar que el proceso de linkeo
+	// termine sin errores. Por tiempo, asumimos que el
+	// resultado fue correcto.
+
+	// Borramos los shaders, porque ya tenemos el ejecutable 
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
 }
 
-void scene_vertex::awake()
-{
+void scene_vertex::awake() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_PROGRAM_POINT_SIZE);
 }
 
-void scene_vertex::sleep()
-{
+void scene_vertex::sleep() {
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+	glDisable(GL_PROGRAM_POINT_SIZE);
 }
 
-void scene_vertex::mainLoop()
-{
+void scene_vertex::mainLoop() {
+
 }
 
-void scene_vertex::resize(int width, int height)
-{
+void scene_vertex::resize(int width, int height) {
+
 }
